4b7cebdcd8c6608afcc8a1ed32123561717f1684
Denis Gibikoski <d3nys.inf@gmail.com>
2019-02-24 19:12:36 -0300

    Subindo Projct Limpo

---
+++ src/main/java/com/example/demo/bean/HomeBean.java
@@ -0,0 +1,26 @@
+package com.example.demo.bean;
+
+import org.primefaces.model.DefaultScheduleModel;
+import org.primefaces.model.ScheduleModel;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HomeBean {
+	
+	private ScheduleModel eventModel = new DefaultScheduleModel();
+
+	public ScheduleModel getEventModel() {
+		return eventModel;
+	}
+
+	public void setEventModel(ScheduleModel eventModel) {
+		this.eventModel = eventModel;
+	}
+
+}
+
+
+
+
+
+
---
+++ src/main/java/com/example/demo/util/FacesUtil.java
@@ -0,0 +1,61 @@
+package com.example.demo.util;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.PrimeFaces;
+
+public class FacesUtil {
+	
+	private static void addMensagem(FacesMessage.Severity tipo, 
+			String mensagem) {
+		FacesContext.getCurrentInstance().addMessage(null, 
+				criarMensagem(tipo, mensagem));
+	}
+	
+	public static void addMensagemErro(String mensagem) {
+		addMensagem(FacesMessage.SEVERITY_ERROR, mensagem);
+	}
+	
+	public static void addMensagemInfo(String mensagem) {
+		addMensagem(FacesMessage.SEVERITY_INFO, mensagem);
+	}
+	
+	private static FacesMessage criarMensagem(FacesMessage.Severity tipo,
+			String mensagem) {
+		return new FacesMessage(tipo, mensagem, null);
+	}
+	
+	public static FacesMessage criarMensagemErro(String mensagem) {
+		return criarMensagem(FacesMessage.SEVERITY_ERROR, mensagem);
+	}
+	
+	public static FacesMessage criarMensagemInfo(String mensagem) {
+		return criarMensagem(FacesMessage.SEVERITY_INFO, mensagem);
+	}
+	
+	private static void executarScriptPrimefaces(String comando) {
+		PrimeFaces.current().executeScript(comando);
+	}
+	
+	public static void abrirDialog(String widgetVar) {
+		executarScriptPrimefaces(String.format("PF('%s').show();", 
+				widgetVar));
+	}
+	
+	public static void fecharDialog(String widgetVar) {
+		executarScriptPrimefaces(String.format("PF('%s').hide();", 
+				widgetVar));
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
